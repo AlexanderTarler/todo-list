2a6b22885b3a117b42bca061b3263408b1636c1f
alex <alexander.tarler@gmail.com>
2023-05-05 12:27:41 +0200

    First commit to set up the repo

---
+++ src/Components/List.tsx
@@ -0,0 +1,20 @@
+
+
+function List() {
+    return (<>
+
+
+    </>);
+}
+
+/*
+
+1) Lägga till en rad i listan genom att hämta data från valfritt api (tex: PokéAPI (pokeapi.co)).
+
+2) Redigera en rad.
+
+3) Ta bort en rad.
+
+*/
+
+export default List;
