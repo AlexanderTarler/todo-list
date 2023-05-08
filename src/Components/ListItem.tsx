57cc34609d347ceccda6f51ea7a8ef2b6626bdd3
alex <alexander.tarler@gmail.com>
2023-05-08 11:39:00 +0200

    Added CSS for the delete button and also made the text for the list-item into an input-field, making the editing of the text into a much smoother user experience

--- src/Components/ListItem.tsx
+++ src/Components/ListItem.tsx
@@ -5,22 +5,19 @@ import React from 'react';
 
 function ListItem({ id, itemToRemove }: any) {
     const [item, setItem] = useState('');
-    const [query, setQuery] = useState('');
     const inputRef = useRef<HTMLInputElement>(null);
 
     const handleChange = (event: any) => {
-        setQuery(event.target.value);
+        setItem(event.target.value);
     }
 
     const handleKeyDown = (event: any) => {
         if (event.key === 'Enter') {
-            setItem(query);
-            setQuery('');
+            event.target.blur();
         }
     };
 
     const editButton = () => {
-        inputRef.current?.classList.toggle('visible');
         inputRef.current?.focus();
     }
 
@@ -32,14 +29,12 @@ function ListItem({ id, itemToRemove }: any) {
     return (<>
         <li key={id} className='list-item'>
             <div>
-                {item}
+                <input ref={inputRef} className='edit-item' type="text" onChange={handleChange} onKeyDown={handleKeyDown} value={item} />
             </div>
 
             <div className='buttons'>
                 <button className='edit-button' onClick={() => editButton()}>Edit</button>
-                <button className='remove-button' onClick={() => itemToRemove(id)}>Remove</button>
-                <input ref={inputRef} className='edit-item' type="text" onChange={handleChange} onKeyDown={handleKeyDown} value={query} />
-
+                <button className="remove-button" onClick={() => itemToRemove(id)}><span className="text">Delete</span><span className="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg></span></button>
             </div>
         </li>
     </>
